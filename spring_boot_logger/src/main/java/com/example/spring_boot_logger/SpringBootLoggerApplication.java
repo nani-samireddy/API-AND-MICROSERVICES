8c9c3a44f77b881f013bd7489d697f511d9a098b
nani-samireddy <nanisamireddy05@gmail.com>
2023-10-23 11:27:58 +0530

    added changes

---
+++ spring_boot_logger/src/main/java/com/example/spring_boot_logger/SpringBootLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggerApplication.class, args);
+    }
+
+}
--- spring_boot_logger/src/test/java/com/example/spring_boot_logger/LoggerController.java
+++ spring_boot_logger/src/test/java/com/example/spring_boot_logger/LoggerController.java
@@ -1,2 +1,17 @@
-package com.example.spring_boot_logger;public class LoggerController {
+package com.example.spring_boot_logger;
+
+@RestController
+public class LoggerController {
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
 }
